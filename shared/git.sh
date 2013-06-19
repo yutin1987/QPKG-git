68c3fd45100c52c15313e333217da777e975cb30
Justin <yuting1987@gmail.com>
2013-06-19 17:10:15 +0800

    Update

--- shared/git.sh
+++ shared/git.sh
@@ -2,6 +2,9 @@
 CONF=/etc/config/qpkg.conf
 QPKG_NAME="git"
 
+PATH_GIT=/mnt/ext/opt/git
+PATH_BIN=/usr/bin
+
 case "$1" in
   start)
     ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
@@ -9,22 +12,22 @@ case "$1" in
         echo "$QPKG_NAME is disabled."
         exit 1
     fi
-    ln -nfs /opt/bin/git /usr/bin/git
-    ln -nfs /opt/bin/git-cvsserver /usr/bin/git-cvsserver
-    ln -nfs /opt/bin/git-receive-pack /usr/bin/git-receive-pack
-    ln -nfs /opt/bin/git-shell /usr/bin/git-shell
-    ln -nfs /opt/bin/git-upload-archive /usr/bin/git-upload-archive
-    ln -nfs /opt/bin/git-upload-pack /usr/bin/git-upload-pack
+    ln -nfs $PATH_GIT/git $PATH_BIN/git
+    ln -nfs $PATH_GIT/git-cvsserver $PATH_BIN/git-cvsserver
+    ln -nfs $PATH_GIT/git-receive-pack $PATH_BIN/git-receive-pack
+    ln -nfs $PATH_GIT/git-shell $PATH_BIN/git-shell
+    ln -nfs $PATH_GIT/git-upload-archive $PATH_BIN/git-upload-archive
+    ln -nfs $PATH_GIT/git-upload-pack $PATH_BIN/git-upload-pack
     : ADD START ACTIONS HERE
     ;;
     
   stop)
-    rm -f /usr/bin/git
-    rm -f /usr/bin/git-cvsserver
-    rm -f /usr/bin/git-receive-pack
-    rm -f /usr/bin/git-shell
-    rm -f /usr/bin/git-upload-archive
-    rm -f /usr/bin/git-upload-pack
+    rm -f $PATH_BIN/git
+    rm -f $PATH_BIN/git-cvsserver
+    rm -f $PATH_BIN/git-receive-pack
+    rm -f $PATH_BIN/git-shell
+    rm -f $PATH_BIN/git-upload-archive
+    rm -f $PATH_BIN/git-upload-pack
     : ADD STOP ACTIONS HERE
     ;;
 
